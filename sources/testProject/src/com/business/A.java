10092cb51d2cbb23d54816ea8a2fb280874ef0e7
kofiproject <kofi.project@gmail.com>
2011-09-18 19:45:49 +0300

    message

--- sources/testProject/src/com/business/A.java
+++
@@ -1,9 +0,0 @@
-package com.business;
-
-/**
- * @author harchevnikov_m
- *         Date: 17.09.11
- *         Time: 20:30
- */
-public class A {
-}
--- sources/testProject/src/com/dataservice/IDataService.java
+++
@@ -1,9 +0,0 @@
-package com.dataservice;
-
-/**
- * User: aaaa
- * Date: 05/06/11
- */
-public interface IDataService {
-    public int getBalanceByCriterion(String criterion);
-}
